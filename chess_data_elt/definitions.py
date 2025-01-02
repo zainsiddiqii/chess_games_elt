925490b90462457cc84a29840e9073d062f59618
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 10:23:34 +0000

    Add Groupings for Assets

--- chess_data_elt/definitions.py
+++ chess_data_elt/definitions.py
@@ -2,8 +2,13 @@ from dagster import Definitions, load_assets_from_modules
 from .resources import bigquery_resource, dbt_resource
 from .assets import google_cloud_storage, dbt
 
-gcs_assets = load_assets_from_modules([google_cloud_storage])
-dbt_assets = load_assets_from_modules([dbt])
+gcs_assets = load_assets_from_modules(
+    [google_cloud_storage],
+    group_name='Extract Load')
+dbt_assets = load_assets_from_modules(
+    [dbt],
+    "Transform"
+)
 
 defs = Definitions(
     assets=[*gcs_assets, *dbt_assets],
