53e5baf77590eecb18d740707cdc247aa2168e6e
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 17:25:24 +0000

    Tried to add dbt_resource from resources to dbt asset

--- chess_data_elt/assets/dbt.py
+++ chess_data_elt/assets/dbt.py
@@ -1,10 +1,11 @@
 from dagster import AssetExecutionContext
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_dbt import dbt_assets
+from ..resources import dbt_resource
 
 from ..project import my_project
 
 @dbt_assets(
     manifest=my_project.manifest_path
 )
-def chess_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["build"], context=context).stream()
+def chess_dbt_assets(context: AssetExecutionContext):
+    yield from dbt_resource.cli(["build"], context=context).stream()
