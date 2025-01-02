75d937e4002a73f30e572337869a9d4ed05a94a9
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 21:35:29 +0000

    Fixed AssetSelection in jobs

--- chess_data_elt/jobs/__init__.py
+++ chess_data_elt/jobs/__init__.py
@@ -4,10 +4,10 @@ from ..partitions import monthly_partition
 monthly_el_update_job = define_asset_job(
     name="monthly_update_job",
     partitions_def=monthly_partition,
-    selection=AssetSelection.groups(["extracting_loading"])
+    selection=AssetSelection.groups("extracting_loading")
 )
 
 monthly_transform_serve_job = define_asset_job(
     name="monthly_transform_serve_job",
-    selection=AssetSelection.groups(["transformation_serving"])
+    selection=AssetSelection.groups("transformation_serving")
 )
