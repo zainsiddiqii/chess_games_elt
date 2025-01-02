44c21c9f3babcf4be58a50089680f14f0f30d249
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 20:44:30 +0000

    Added job and schedule to assets

--- chess_data_elt/definitions.py
+++ chess_data_elt/definitions.py
@@ -1,6 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 from .resources import bigquery_resource, dbt_resource
 from .assets import dbt_and_looker, google_cloud_storage
+from .jobs import monthly_update_job
+from .schedules import monthly_update_schedule
 
 gc_assets = load_assets_from_modules(
     [google_cloud_storage],
@@ -11,11 +13,12 @@ dbt_and_looker_assets = load_assets_from_modules(
     group_name="transformation_serving"
 )
 
-
 defs = Definitions(
     assets=[*gc_assets, *dbt_and_looker_assets,],
     resources={
         "bigquery": bigquery_resource,
         "dbt": dbt_resource,
     },
+    jobs=[monthly_update_job],
+    schedules=[monthly_update_schedule],
 )
