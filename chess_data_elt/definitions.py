1ec5ae807390f0dbbbb06a22f03c23f098eef8ba
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 21:39:10 +0000

    Fixed Naming Issue with Job Imports

--- chess_data_elt/definitions.py
+++ chess_data_elt/definitions.py
@@ -1,7 +1,7 @@
 from dagster import Definitions, load_assets_from_modules
 from .resources import bigquery_resource, dbt_resource
 from .assets import dbt_and_looker, google_cloud_storage
-from .jobs import monthly_el_update_job
+from .jobs import monthly_el_update_job, monthly_transform_serve_job
 from .schedules import monthly_update_schedule
 from .sensors import transformation_sensor
 
@@ -20,7 +20,7 @@ defs = Definitions(
         "bigquery": bigquery_resource,
         "dbt": dbt_resource,
     },
-    jobs=[monthly_el_update_job],
+    jobs=[monthly_el_update_job, monthly_transform_serve_job],
     schedules=[monthly_update_schedule],
     sensors=[transformation_sensor],
 )
