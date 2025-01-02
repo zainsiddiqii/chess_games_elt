afc89c294ecad56bc1f297f6f753f8950e3591f8
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-02 19:50:23 +0000

    Changed dbt target to be dynamic based on Dagster Deployment

--- chess_data_elt/project.py
+++ chess_data_elt/project.py
@@ -1,12 +1,19 @@
 from pathlib import Path
-
+import os
 from dagster_dbt import DbtProject
 
 RELATIVE_PATH_TO_MY_DBT_PROJECT = "../dbt_chess_games"
+
+def get_env():
+    if os.getenv("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT", "") == "1":
+        return "test"
+    if os.getenv("DAGSTER_CLOUD_DEPLOYMENT_NAME", "") == "prod":
+        return "prod"
 
 my_project = DbtProject(
     project_dir=Path(__file__)
     .joinpath("..", RELATIVE_PATH_TO_MY_DBT_PROJECT)
     .resolve(),
+    target=get_env(),
 )
 my_project.prepare_if_dev()
