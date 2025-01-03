7a4a17d7f55a41fe1963ac3d9f2ae386f3963d14
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-03 17:08:39 +0000

    added looker asset

--- chess_data_elt/definitions.py
+++ chess_data_elt/definitions.py
@@ -1,6 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 from .resources import bigquery_resource, dbt_resource
-from .assets import dbt_and_looker, google_cloud_storage
+from .assets import dbt_and_looker, google_cloud_storage, looker
 from .jobs import monthly_el_update_job, monthly_transform_serve_job
 from .schedules import monthly_update_schedule
 from .sensors import bigquery_raw_table_sensor
@@ -13,9 +13,13 @@ dbt_and_looker_assets = load_assets_from_modules(
     [dbt_and_looker],
     group_name="transformation_serving"
 )
+looker_assets = load_assets_from_modules(
+    [looker],
+    group_name="serving"
+)
 
 defs = Definitions(
-    assets=[*gc_assets, *dbt_and_looker_assets,],
+    assets=[*gc_assets, *dbt_and_looker_assets,*looker_assets],
     resources={
         "bigquery": bigquery_resource,
         "dbt": dbt_resource,
