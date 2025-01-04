b53fd287616d50929510f34b10a8266b3a65b670
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-04 13:52:01 +0000

    Fixed project.py dbt file path

--- elt_chess_games/project.py
+++ elt_chess_games/project.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-
 from dagster_dbt import DbtProject
 
 chess_games_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..", "dbt_chess_games").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..", "dbt_chess_games").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
 )
 chess_games_project.prepare_if_dev()
