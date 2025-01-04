d04b3fec4ffb29a2aac8f7163a26797c68ef5ac9
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-04 14:55:05 +0000

    changed python version from 3.10 to 3.12 for deployment

--- elt_chess_games/assets/utils.py
+++ elt_chess_games/assets/utils.py
@@ -2,7 +2,6 @@ import polars as pl
 import requests
 from polars.exceptions import SchemaError
 from datetime import datetime
-import time
 
 from google.cloud.bigquery import (
     LoadJobConfig,
@@ -29,7 +28,6 @@ def get_monthly_archive(year: str, month: str, username: str) -> dict:
     base_url = f"https://api.chess.com/pub/player/{username}/games/"
     url = f"{base_url}{year}/{month}"
     response = requests.get(url, headers=headers)
-    time.sleep(5)
     response_json = response.json()
     
     return response_json
