7edf150e74914df8b1306407f3f472c2852d8a4b
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-05 21:05:43 +0000

    trying without CustomizedDbtTranslater

--- elt_chess_games/assets/dbt.py
+++ elt_chess_games/assets/dbt.py
@@ -16,8 +16,7 @@ class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
 
 
 @dbt_assets(
-    manifest=chess_games_project.manifest_path,
-    dagster_dbt_translator=CustomizedDagsterDbtTranslator()
+    manifest=chess_games_project.manifest_path
 )
 def chess_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     service_account_key_path = store_service_account_key(context)
