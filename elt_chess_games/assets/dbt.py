9a6430bcce3ed7c060b1eb0645b36dc8f7d9f308
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-05 20:27:52 +0000

    added context argument to store_service_account_key

--- elt_chess_games/assets/dbt.py
+++ elt_chess_games/assets/dbt.py
@@ -8,7 +8,7 @@ import os
     manifest=chess_games_project.manifest_path,
 )
 def chess_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    service_account_key_path = store_service_account_key()
+    service_account_key_path = store_service_account_key(context)
     # Set the environment variable for dbt to use the file path
     os.environ["GCP_SERVICE_ACCOUNT_KEYFILE"] = service_account_key_path
     yield from dbt.cli(["build"], context=context).stream()
