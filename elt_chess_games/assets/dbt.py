e52f18e1238c97d643373ca0d393abb9c78f64e7
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-05 21:00:17 +0000

    changes dbt to use DagsterDbtTranslator to interact with non-dbt assets

--- elt_chess_games/assets/dbt.py
+++ elt_chess_games/assets/dbt.py
@@ -1,11 +1,23 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
 from ..project import chess_games_project
 from .utils import store_service_account_key
 import os
 
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        if resource_type == "source":
+            return AssetKey(f"bigquery_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+
+
+
 @dbt_assets(
     manifest=chess_games_project.manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator()
 )
 def chess_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     service_account_key_path = store_service_account_key(context)
