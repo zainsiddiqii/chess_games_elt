a1820dbf5fa6f010fc269df2eb45171ded5ebcfb
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-05 09:19:12 +0000

    Renamed el job from monthly_update_job -> monthly_extract_load_job

--- elt_chess_games/jobs/__init__.py
+++ elt_chess_games/jobs/__init__.py
@@ -2,7 +2,7 @@ from dagster import define_asset_job, AssetSelection
 from ..partitions import monthly_partition
 
 monthly_el_update_job = define_asset_job(
-    name="monthly_update_job",
+    name="monthly_extract_load_job",
     partitions_def=monthly_partition,
     selection=AssetSelection.groups("extracting_loading")
 )
