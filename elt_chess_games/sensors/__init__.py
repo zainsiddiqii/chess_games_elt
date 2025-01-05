8b6f1ab125909add6669e411ef4f1fa3ec7c4aa9
Zain Siddiqi <zainsiddiqi@randa.org>
2025-01-05 18:48:28 +0000

    fixed bigquery_raw_table_sensor

--- elt_chess_games/sensors/__init__.py
+++ elt_chess_games/sensors/__init__.py
@@ -2,16 +2,15 @@ from ..jobs import monthly_transform_serve_job
 from dagster import (
     AssetKey,
     EventLogEntry,
-    RunConfig,
     SensorEvaluationContext,
     RunRequest,
     asset_sensor,
 )
 
 # AssetKey for the asset to monitor
-BIGQUERY_RAW_TABLE_ASSET = AssetKey("bigquery_raw_games_chesscom")
+bigquery_raw_games_chesscom_asset_key = AssetKey("bigquery_raw_games_chesscom")
 
-@asset_sensor(asset_key=BIGQUERY_RAW_TABLE_ASSET, job=monthly_transform_serve_job)
+@asset_sensor(asset_key=bigquery_raw_games_chesscom_asset_key, job=monthly_transform_serve_job)
 def bigquery_raw_table_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
     assert asset_event.dagster_event and asset_event.dagster_event.asset_key
     yield RunRequest(
@@ -24,5 +23,6 @@ def bigquery_raw_table_sensor(context: SensorEvaluationContext, asset_event: Eve
                     }
                 }
             }
-        },
+        }
+
     )
