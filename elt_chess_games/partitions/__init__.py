6e9186c09106c1d832c1806f1e6a4b048ba6295d
Zain Siddiqi <zainsiddiqi@randa.org>
2025-02-02 11:57:01 +0000

    Removed cron schedule from monthly update schedule

--- elt_chess_games/partitions/__init__.py
+++ elt_chess_games/partitions/__init__.py
@@ -5,6 +5,5 @@ start_date = constants.START_DATE
 end_date = constants.END_DATE
 
 monthly_partition = MonthlyPartitionsDefinition(
-    start_date=start_date,
-    end_date=end_date
+    start_date=start_date
 )
--- elt_chess_games/schedules/__init__.py
+++ elt_chess_games/schedules/__init__.py
@@ -3,5 +3,4 @@ from ..jobs import monthly_el_update_job
 
 monthly_update_schedule = build_schedule_from_partitioned_job(
     job=monthly_el_update_job,
-    cron_schedule="0 0 1 * *", # every 1st of the month at midnight
 )
