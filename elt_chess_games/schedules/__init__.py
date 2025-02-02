d707096f8fe3954e21620a85949153e35e8d5221
Zain Siddiqi <zainsiddiqi@randa.org>
2025-02-02 11:50:21 +0000

    Changed monthly extract load schedule to partitioned job

--- elt_chess_games/schedules/__init__.py
+++ elt_chess_games/schedules/__init__.py
@@ -1,7 +1,7 @@
-from dagster import ScheduleDefinition
+from dagster import ScheduleDefinition, build_schedule_from_partitioned_job
 from ..jobs import monthly_el_update_job
 
-monthly_update_schedule = ScheduleDefinition(
+monthly_update_schedule = build_schedule_from_partitioned_job(
     job=monthly_el_update_job,
     cron_schedule="0 0 1 * *", # every 1st of the month at midnight
 )
